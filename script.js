5c7dd5df6bb722fcb1352881e057fefdd28e2417
Kaua <goncalveskaua2k24@gmail.com>
2025-04-14 17:13:15 -0300

    PROJETO BASICA PARA TREINAR

---
+++ script.js
@@ -0,0 +1,22 @@
+function converter() {
+    const temperatura = parseFloat(document.getElementById('temperature').value);
+    const tipo = document.getElementById('conversionType').value;
+    const resultado = document.getElementById('resultado');
+
+    if (isNaN(temperatura)) {
+
+    resultado.textContent = 'Por favor, insira um número válido.';
+    return;
+
+}
+
+        if (tipo === 'CtoF') {
+            
+          const fahrenheit = (temperatura * 9/5) + 32;
+        resultado.textContent = `${temperheit.toFixed(2)} °F`;
+        } else {
+            
+          const celsius = (temperatura - 32) * 5/9;
+        resultado.textContent = `${celsius.toFixed(2)} °C`;
+    }
+}
